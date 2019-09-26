00741d4c971fcd6bb4d131350556574ddc7e5a2f
Matthew Halpenny <matt.halpenny@gmail.com>
2019-09-26 00:31:35 -0400

    Basic Layout

---
+++ js/somme.js
@@ -0,0 +1,14 @@
+// setTimeout(function() {
+//   document.getElementById('intro').style.opacity='0';
+// }, 1*1000);
+
+function samOn(){
+$('.somme').hide();
+$('#sam').show();
+$('#sam').text('Sam Bourgault');
+}
+
+function samOff(){
+  $('.somme').show();
+  $('#sam').text('S');
+  }
