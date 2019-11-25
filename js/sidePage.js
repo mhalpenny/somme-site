e29c3cbd494e23872644fe4533d10b6f2b0d5de8
Matthew Halpenny <matt.halpenny@gmail.com>
2019-11-24 23:46:38 -0500

    New multiplexer png

--- js/sidePage.js
+++ js/sidePage.js
@@ -1,4 +1,23 @@
 function leftPage(){
-$("#leftPage").show();
+$("#leftPage").toggle();
+$("#home").toggle();
+var color = $("#headerBar").css("background-color");
+if(color == 'rgb(255, 255, 255)'){
+$("#headerBar").css("background-color", "transparent");
+$("#leftPage").animate({
+  left: '-100%'
+}, 600, function() {
+  $("#leftPage").css('left', '-100%');
+});
+}else if(color == 'rgba(0, 0, 0, 0)'){
+  $("#headerBar").css("background-color", "white");
+  $("#leftPage").animate({
+    left: '0%'
+  }, 600, function() {
+    $(this).css('left', '0%');
+  });
+  }
+  console.log(color);
 }
+
 
