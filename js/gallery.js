36e46d3b2f9836377cc884ec9882609d89712477
Matthew Halpenny <matt.halpenny@gmail.com>
2020-01-13 01:11:20 -0500

    Mobile V2 // Fixed galleries

--- js/gallery.js
+++ js/gallery.js
@@ -1,7 +1,97 @@
-$(function(){
-  var gallery1 = $('.gallery-1 a').simpleLightbox();
+// $(function(){
+//   var gallery1 = $('.gallery-1 a').simpleLightbox();
 
-  gallery1.on('show.simplelightbox', function(){
+//   gallery1.on('show.simplelightbox', function(){
+//     console.log('Requested for showing');
+//   })
+//   .on('shown.simplelightbox', function(){
+//     console.log('Shown');
+//   })
+//   .on('close.simplelightbox', function(){
+//     console.log('Requested for closing');
+//   })
+//   .on('closed.simplelightbox', function(){
+//     console.log('Closed');
+//   })
+//   .on('change.simplelightbox', function(){
+//     console.log('Requested for change');
+//   })
+//   .on('next.simplelightbox', function(){
+//     console.log('Requested for next');
+//   })
+//   .on('prev.simplelightbox', function(){
+//     console.log('Requested for prev');
+//   })
+//   .on('nextImageLoaded.simplelightbox', function(){
+//     console.log('Next image loaded');
+//   })
+//   .on('prevImageLoaded.simplelightbox', function(){
+//     console.log('Prev image loaded');
+//   })
+//   .on('changed.simplelightbox', function(){
+//     console.log('Image changed');
+//   })
+//   .on('nextDone.simplelightbox', function(){
+//     console.log('Image changed to next');
+//   })
+//   .on('prevDone.simplelightbox', function(){
+//     console.log('Image changed to prev');
+//   })
+//   .on('error.simplelightbox', function(e){
+//     console.log('No image found, go to the next/prev');
+//     console.log(e);
+//   });
+// });
+
+// $(function(){
+//   var gallery2 = $('.gallery-2 a').simpleLightbox();
+
+//   gallery2.on('show.simplelightbox', function(){
+//     console.log('Requested for showing');
+//   })
+//   .on('shown.simplelightbox', function(){
+//     console.log('Shown');
+//   })
+//   .on('close.simplelightbox', function(){
+//     console.log('Requested for closing');
+//   })
+//   .on('closed.simplelightbox', function(){
+//     console.log('Closed');
+//   })
+//   .on('change.simplelightbox', function(){
+//     console.log('Requested for change');
+//   })
+//   .on('next.simplelightbox', function(){
+//     console.log('Requested for next');
+//   })
+//   .on('prev.simplelightbox', function(){
+//     console.log('Requested for prev');
+//   })
+//   .on('nextImageLoaded.simplelightbox', function(){
+//     console.log('Next image loaded');
+//   })
+//   .on('prevImageLoaded.simplelightbox', function(){
+//     console.log('Prev image loaded');
+//   })
+//   .on('changed.simplelightbox', function(){
+//     console.log('Image changed');
+//   })
+//   .on('nextDone.simplelightbox', function(){
+//     console.log('Image changed to next');
+//   })
+//   .on('prevDone.simplelightbox', function(){
+//     console.log('Image changed to prev');
+//   })
+//   .on('error.simplelightbox', function(e){
+//     console.log('No image found, go to the next/prev');
+//     console.log(e);
+//   });
+// });
+
+$(function(){
+  var $gallery1 = $('.gallery-1 a').simpleLightbox();
+
+  $gallery1.on('show.simplelightbox', function(){
     console.log('Requested for showing');
   })
   .on('shown.simplelightbox', function(){
@@ -44,9 +134,9 @@ $(function(){
 });
 
 $(function(){
-  var gallery3 = $('.gallery-2 a').simpleLightbox();
-
-  gallery3.on('show.simplelightbox', function(){
+  var $gallery2 = $('.gallery-2 a').simpleLightbox();
+  
+  $gallery2.on('show.simplelightbox', function(){
     console.log('Requested for showing');
   })
   .on('shown.simplelightbox', function(){
